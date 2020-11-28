120750270953eb169f5cda5ba0916d97107d3c7c
kevinzhu9181 <74467988+kevinzhu9181@users.noreply.github.com>
2020-11-28 10:03:04 -0600

    Last Update of Project Mini Compiler

---
+++ Quadruple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    public class Quadruple
+    {
+        public string Operation { get; set; }
+        public string StrLeft { get; set; }
+        public string StrRight { get; set; }
+        public string Result { get; set; }
+        public bool Input { get; set; }
+        public Quadruple() { }
+        public Quadruple(string operation, string strLeft, string strRight, string result)
+        {
+            this.Operation = operation;
+            this.StrLeft = strLeft;
+            this.StrRight = strRight;
+            this.Result = result;
+            this.Input = false;
+        }
+    }
+}
